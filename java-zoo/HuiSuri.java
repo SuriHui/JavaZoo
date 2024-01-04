43a0d92566f10403b681e8e7bd77256f3891aec5
SuriHui <143148869+SuriHui@users.noreply.github.com>
2024-01-04 11:15:07 -0800

    Add files via upload

---
+++ java-zoo/HuiSuri.java
@@ -0,0 +1,28 @@
+/**
+ * Write a description of class HuiSuri here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HuiSuri extends Animal
+{
+    public HuiSuri() {
+        super("Suri Hui","purple",4);
+    }
+    
+    public String getType() {
+        return "Rabunny";   
+    }
+    
+    public String getName() {
+        return "LeafChicken";
+    }
+    
+    public String speak() {
+        return "ORANG GOOS!!";
+    }
+    
+    public String getDisposition() {
+        return "evil";
+    }
+}
